0b3dc7c1aa30a27c79dd66c48dbccdc905055ba2
AnihiliusA <26289415+anihiliusa@users.noreply.github.com>
2026-05-29 12:55:55 +0300

    Add Xtube boot receiver

---
+++ app/src/main/java/com/anihiliusa/xtube/BootReceiver.kt
@@ -0,0 +1,16 @@
+package com.anihiliusa.xtube
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (intent?.action != Intent.ACTION_BOOT_COMPLETED) return
+        if (!AppPreferences.autoStart(context)) return
+        if (!AppPreferences.keepService(context)) return
+        val service = Intent(context, MediaKeepAliveService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) context.startForegroundService(service) else context.startService(service)
+    }
+}
