4a31ad7419439024ff7880febfd75798a549d257
AnihiliusA <26289415+anihiliusa@users.noreply.github.com>
2026-05-29 16:14:43 +0300

    Add Xtube flags

---
+++ app/src/isolated/java/com/anihiliusa/xtube/Flags.java
@@ -0,0 +1,9 @@
+package com.anihiliusa.xtube;
+
+public final class Flags {
+    public static boolean cosmetic = true;
+    public static boolean network = true;
+    public static boolean vpn = false;
+    public static boolean background = true;
+    private Flags() {}
+}
